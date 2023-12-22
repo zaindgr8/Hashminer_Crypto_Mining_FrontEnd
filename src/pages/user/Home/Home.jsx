bfbef5d1678f11142c25c3fe130e7513be9d2587
zaindgr8 <66069515+zaindgr8@users.noreply.github.com>
2023-12-23 01:53:24 +0500

    variety3

--- src/pages/user/Home/Home.jsx
+++ src/pages/user/Home/Home.jsx
@@ -1,52 +1,31 @@
 import React from "react";
-import Navbar from "../Navbar/Navbar";
-import Sidebar from "../Sidebar/Sidebar";
-import Card from "../Cards/deposit_card";
-import Card1 from "../Cards/withdraw_card";
-import Card2 from "../Cards/nobutton_card";
-import Card4 from "../Cards/package_card";
-import Comp from "./Comp";
-import "./Home.scss";
 
-function Home({ user }) {
+const HelloWorldScreen = () => {
+  // Add event listener to remove body overflow on mount
+  React.useEffect(() => {
+    // On mount
+    document.body.style.overflow = "hidden";
+    // On unmount
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, []);
+
   return (
-    <div className="relative w-screen h-screen overflow-hidden">
-      {/* Navbar */}
-      <Navbar className="absolute w-full z-10" user={user} />
+    <div className="flex h-screen overflow-hidden">
+      {/* Sidebar */}
+      <div className="w-1/4 bg-gray-800 text-white p-4">
+        {/* Sidebar content goes here */}
+        Sidebar Content
+      </div>
 
       {/* Main Content */}
-      <div className="flex h-full">
-        <Sidebar />
-        <div className="flex flex-col p-8">
-          <div className="flex gap-x-5">
-            <Card
-              user={user}
-              title="Available Balance"
-              description="5.00"
-              button="Deposit Now"
-            />
-            <Card1
-              title="Available Withdrawal"
-              description="00.00"
-              button="Withdraw Now"
-            />
-            <Card4
-              title="Package Status"
-              description="Invested: "
-              description1="Package: "
-              button="Upgrade"
-            />
-            <Card2
-              title="Referral Balance"
-              description="0"
-              button="Withdraw Now"
-            />
-          </div>
-          <Comp user={user} />
-        </div>
+      <div className="w-3/4 flex items-center justify-center bg-gray-200">
+        {/* Centered Hello, World! */}
+        <div className="text-4xl">Hello, World!</div>
       </div>
     </div>
   );
-}
+};
 
-export default Home;
+export default HelloWorldScreen;
